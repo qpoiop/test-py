46ec89f072347c72e3a011e8a49a6f1fb8b03457
qpoiop <qpoiop1@gmail.com>
2019-02-18 14:17:31 +0900

    host 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '*y4gyu_8@cu(l$@@9cq&3&8lby+g#x^)r=qw_+z^0xq^yt-u-2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
